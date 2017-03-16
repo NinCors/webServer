be2d5b42c21a045ad1286f341349e3563f01facb
chiyu <chiyuc2@gmail.com>
2017-03-15 22:55:57 -0700

    create server file and implement first get function

---
+++ server.py
@@ -0,0 +1,30 @@
+import BaseHTTPServer
+
+class requestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    '''
+        Handle the request and return page
+    '''
+
+    # build the page model
+    page = '''\
+        <html>
+        <body>
+        <p>Hello,wwwworlllld!!!</p>
+        </body>
+        </html>
+
+    '''
+
+    # deal with one get request
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "text/html")
+        self.send_header("Content-Length",str(len(self.Page)))
+        self.end_headers()
+        self.wfile.write(self.Page)
+
+
+if __name__ == '__main__':
+    serverAddress = ('', 8080)
+    server = BaseHTTPServer.HTTPServer(serverAddress, RequestHandler)
+    server.serve_forever()
