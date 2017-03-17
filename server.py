08b93053b2b827a5c7f357b4e28a1afb769c89f5
chiyu <chiyuc2@gmail.com>
2017-03-17 15:07:23 -0700

    add error handler exception

--- server.py
+++ server.py
@@ -1,30 +1,48 @@
-import BaseHTTPServer
+import BaseHTTPServer,sys,os
 
 class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     '''
         Handle the request and return page,inherit from BaseHTTPRequestHandler.
     '''
 
-    # build the page model
-    Page = '''\
+    # build the page 
+    Error_Page = '''\
         <html>
-        <body>
-            <table>
-                <tr>  <td>Header</td>         <td>Value</td>          </tr>
-                <tr>  <td>Date and time</td>  <td>{date_time}</td>    </tr>
-                <tr>  <td>Client host</td>    <td>{client_host}</td>  </tr>
-                <tr>  <td>Client port</td>    <td>{client_port}</td> </tr>
-                <tr>  <td>Command</td>        <td>{command}</td>      </tr>
-                <tr>  <td>Path</td>           <td>{path}</td>         </tr>
-            </table>
-        </body>
+            <body>
+                <h1>Error accessing {path}</h1>
+                <p>{msg}</p>
+            </body>
         </html>
         '''
 
     # The get request
     def do_GET(self):
-        page = self.create_page()
-        self.send_content(page)
+        try:
+            full_path = os.getcwd() + self.path
+            if not os.path.exists(full_path):
+                raise ServerException("'{0}' not found".format(self.path))
+
+            elif os.path.isfile(full_path):
+                self.handle_file(full_path)
+
+            else:
+                raise ServerException("Unkown object '{0}'".format(self.path))
+
+        except Exception as msg:
+            self.handle_error(msg)
+    
+    def handle_file(self,full_path):
+        try:
+            with open(full_path,'rb') as file:
+                content = file.read()
+            self.send_content(content)
+        except IOError as msg:
+            msg = "'{0}' cannot be read: {1}".format(self.path,msg)
+            self.handle_error(msg)
+
+    def handle_error(self,msg): 
+        content = self.Error_Page.format(path=self.path, msg=msg)
+        self.send_content(content,404)
 
     def create_page(self):
         values = {
@@ -38,12 +56,18 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         return page
 
     # Send the content based on the request
-    def send_content(self,page):
-        self.send_response(200)
+    def send_content(self,page,status=200):
+        self.send_response(status)
         self.send_header("Content-Type", "text/html")
-        self.send_header("Content-Length",str(len(self.Page)))
+        self.send_header("Content-Length",str(len(page)))
         self.end_headers()
-        self.wfile.write(self.Page)        
+        self.wfile.write(page)        
+
+# error handler
+class ServerException(Exception):
+    pass
+
+
 
 if __name__ == '__main__':
     serverAddress = ('', 8080)
